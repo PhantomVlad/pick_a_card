030b9df45a7f6154cffabb0bb2eff64d0f111b05
PhantomVlad <mmvlad99@mail.ru>
2022-10-21 18:40:00 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 values = %w[2 3 4 5 6 7 8 9 10 Валет Король Дама Туз]
-suits = %w[буби черви крести пики]
+suits = ["\u2660".encode('utf-8'), "\u2665".encode('utf-8'), "\u2666".encode('utf-8'), "\u2663".encode('utf-8')]
 
-puts "#{values.sample} #{suits.sample
+puts "#{values.sample}#{suits.sample}"
 #Комментарии

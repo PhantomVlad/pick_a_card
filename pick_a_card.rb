f0ed5b4c31392d2143b76aabe3d40ea4a2464c74
PhatomVlad <116370358+PhatomVlad@users.noreply.github.com>
2022-10-21 18:24:15 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,5 @@
 values = %w[2 3 4 5 6 7 8 9 10 Валет Король Дама Туз]
 suits = %w[буби черви крести пики]
 
-puts "#{values.sample} #{suits.sample}"
+puts "#{values.sample} #{suits.sample
+#Комментарии
